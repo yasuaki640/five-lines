e36c44cee92fa9d43ce3f258f5a86a7e965ba3f6
yasuaki640 <yasuaki640@gmail.com>
2026-05-09 12:46:28 +0900

    Inline drawTile into drawMap (Rule 1 / Inline Method)
    drawTile became a single-line wrapper after Push Code Into Classes,
    so it no longer adds readability. Inline its body into drawMap.

--- index.ts
+++ index.ts
@@ -824,13 +824,9 @@ function drawPlayer(g: CanvasRenderingContext2D) {
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      drawTile(g, x, y);
+      map[y][x].draw(g, x, y);
     }
   }
-}
-
-function drawTile(g: CanvasRenderingContext2D, x: number, y: number) {
-  map[y][x].draw(g, x, y);
 }
 
 function gameLoop() {
