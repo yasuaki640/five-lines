c059019db1ecc09c737ac5fddbd5d7dcc91645aa
yasuaki640 <yasuaki640@gmail.com>
2026-05-17 09:53:51 +0900

    Combine isStony/isBoxy drop ifs with || in updateTitle (Combine Ifs)

--- index.ts
+++ index.ts
@@ -932,11 +932,8 @@ function updateMap() {
 }
 
 function updateTitle(y: number, x: number) {
-  if (map[y][x].isStony() && map[y + 1][x].isAir()) {
-    map[y][x].drop();
-    map[y + 1][x] = map[y][x];
-    map[y][x] = new Air();
-  } else if (map[y][x].isBoxy() && map[y + 1][x].isAir()) {
+  if ((map[y][x].isStony() && map[y + 1][x].isAir())
+    || (map[y][x].isBoxy() && map[y + 1][x].isAir())) {
     map[y][x].drop();
     map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
